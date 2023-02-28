7bf4f6c616bbf4d9671c85ce9e15253bd7b6ce9e
Reza Karamad <reza.karamad@idealo.de>
2023-02-28 22:39:18 +0100

    Release version v0.0.1
    This commit includes a new path.
    
    Changes:
    - Added shopping path

--- cmd/hellofromgo/main.go
+++ cmd/hellofromgo/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-        "fmt"
-        "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Hello from Go.")
-        })
-        http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Home sweet home.")
-        })
-        http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Go.")
+	})
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Home sweet home.")
+	})
+	http.HandleFunc("/shopping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Run.")
+	})
+	http.ListenAndServe(":8080", nil)
 }
